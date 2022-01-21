bf55d7b89dcf619f6125cbd673902d02c2efba14
mellowmorgan <morgan.waites@gmail.com>
2022-01-21 08:29:49 -0800

    add dog cats tables, controllers

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,44 @@
+class CatsController < ApplicationController
+  def random
+    @cats = Cat.all.shuffle.sample
+    json_response(@cats)
+  end
+  
+  def index
+    @cats = Cat.all
+    json_response(@cats)
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    json_response(@cat)
+  end
+
+  def create
+    @cat = Cat.create!(cat_params)
+    json_response(@cat, :created)
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update!(cat_params)
+      render status: 200, json: {
+      message: "This cat has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @cat = Cat.find(params[:id])
+    if @cat.destroy
+      render status: 200, json: {
+      message: "This cat has been deleted successfully."
+      }
+    end
+  end
+
+
+  def cat_params
+    params.permit(:content)
+  end
+end
---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,44 @@
+class DogsController < ApplicationController
+  def random
+    @dogs = Dog.all.shuffle.sample
+    json_response(@dogs)
+  end
+  
+  def index
+    @dogs = Dog.all
+    json_response(@dogs)
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    json_response(@dog)
+  end
+
+  def create
+    @dog = Dog.create!(dog_params)
+    json_response(@dog, :created)
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    if @dog.update!(dog_params)
+      render status: 200, json: {
+      message: "This dog has been updated successfully."
+      }
+    end
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    if @dog.destroy
+      render status: 200, json: {
+      message: "This dog has been deleted successfully."
+      }
+    end
+  end
+
+
+  def dog_params
+    params.permit(:content)
+  end
+end
