a7819b6f28e21e720effec1d5fc2050aed9ba0ea
mellowmorgan <morgan.waites@gmail.com>
2022-01-21 10:55:08 -0800

    WIP: activestorage add image

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,5 @@
 class Cat < ApplicationRecord
+  #no validations because commonly an animal might be entered into shelter with unknown attributes, no name
   scope :search_breed, -> (breed_parameter) { where(breed: breed_parameter) }
+  has_one_attached :image
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,4 @@
 class Dog < ApplicationRecord
   scope :search_breed, -> (breed_parameter) { where(breed: breed_parameter) }
+  has_one_attached :image
 end
