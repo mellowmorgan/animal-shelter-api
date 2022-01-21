2b3ea5094743b74eeca589bebe8a102c39a8d511
mellowmorgan <morgan.waites@gmail.com>
2022-01-21 08:53:16 -0800

    database seeded

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.6'
 # Use postgresql as the database for Active Record
@@ -27,10 +26,15 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
-
+gem 'faker'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'shoulda-matchers', '~> 3.1'
+  gem 'pry'
+
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dog.destroy_all
+Cat.destroy_all
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_cats
+    seed.generate_dogs
+  end
+
+  def generate_cats
+    50.times do |i|
+      cat = Cat.create!(
+        name: Faker::Creature::Cat.name,
+        breed: Faker::Creature::Cat.breed, age: rand(1..21)
+      )
+      puts "Cat #{i}: name #{cat.name} and breed is '#{cat.breed}' and age is #{cat.age}."
+    end
+  end
+  def generate_dogs
+    20.times do |i|
+      dog = Dog.create!(
+        name: Faker::Creature::Dog.name,
+        breed: Faker::Creature::Dog.breed, age: rand(1..21)
+      )
+      puts "Dog #{i}: name #{dog.name} and breed is '#{dog.breed}' and age is #{dog.age}."
+    end
+  end
+end
+
+Seed.begin
